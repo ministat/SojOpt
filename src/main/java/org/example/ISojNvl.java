bfdd014337154eae79299260f61e7c56dfad4b19
Hongjiang Zhang <hongjizhang@ebay.com>
2020-03-12 23:56:12 +0800

    investigate how to improve SOJ lib perf

---
+++ src/main/java/org/example/ISojNvl.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public interface ISojNvl {
+    public String getTagValue(String value, String key);
+    public String ToString();
+}
---
+++ src/main/java/org/example/JdkSojNvlImpl.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JdkSojNvlImpl implements ISojNvl {
+    public static final String[] KV_DELIMITER = new String[]{"&", "&_", "&!"};
+    public static final String BLANK_STRING = "";
+    public JdkSojNvlImpl()
+    {
+    }
+    public String getTagValue(String value, String key) {
+        if (!StringUtils.isBlank(value) && !StringUtils.isBlank(key)) {
+            value = "&" + value;
+            String kvSet = "";
+            String keySet = "";
+
+            int startpos;
+            for(startpos = 0; startpos < KV_DELIMITER.length; ++startpos) {
+                keySet = keySet + KV_DELIMITER[startpos] + key + "=|";
+                kvSet = kvSet + KV_DELIMITER[startpos] + "|";
+            }
+
+            keySet = keySet.substring(0, keySet.length() - 1);
+            kvSet = kvSet.substring(0, kvSet.length() - 1);
+            Pattern p1 = Pattern.compile(keySet);
+            Pattern p2 = Pattern.compile(kvSet);
+            Matcher m1 = p1.matcher(value);
+            Matcher m2 = p2.matcher(value);
+            if (m1.find()) {
+                startpos = m1.start();
+                int tmppos = m1.end();
+                int endpos;
+                if (m2.find(tmppos)) {
+                    endpos = m2.start();
+                } else {
+                    endpos = value.length();
+                }
+
+                if (endpos < 0) {
+                    endpos = value.length() - 1;
+                }
+
+                String[] kvPair = value.substring(startpos, endpos).split("=", 2);
+                return "".equals(kvPair[1]) ? null : kvPair[1];
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public String ToString() {
+        return getClass().getName();
+    }
+}
